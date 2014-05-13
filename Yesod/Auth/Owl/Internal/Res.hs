a517724ffbb42326c7c69468c42e791a79e69df6
cutsea110 <cutsea110@gmail.com>
2014-05-13 18:44:41 +0900

    TODO: I expect future aeson library
    ref. https://github.com/bos/aeson/issues/187

--- Yesod/Auth/Owl/Internal/Res.hs
+++ Yesod/Auth/Owl/Internal/Res.hs
@@ -3,11 +3,20 @@ module Yesod.Auth.Owl.Internal.Res where
 import Control.Applicative ((<$>),(<*>))
 import Control.Monad (mzero)
 import Data.Aeson
+import qualified Data.Text.Lazy  as TL
 import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.Text.Lazy.Encoding as TE
+import qualified Data.ByteString.Base64.Lazy as Base64
 
-data OwlRes = OwlRes { cipher :: LB.ByteString }
+encodeToText :: LB.ByteString -> TL.Text
+encodeToText = TE.decodeUtf8 . Base64.encode
+
+decodeFromText :: (Monad m) => TL.Text -> m LB.ByteString
+decodeFromText = either fail return . Base64.decode . TE.encodeUtf8
+
+data OwlRes = OwlRes { cipher :: LB.ByteString } deriving Show
 instance FromJSON OwlRes where
-  parseJSON (Object o) = OwlRes <$> o .: "cipher"
+  parseJSON (Object o) = OwlRes <$> ((o .: "cipher") >>= decodeFromText)
   parseJSON _ = mzero
 instance ToJSON OwlRes where
-  toJSON (OwlRes e) = object [ "cipher" .= e ]
+  toJSON (OwlRes e) = object [ "cipher" .= encodeToText e ]
